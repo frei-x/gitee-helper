dcae79bf549770c291a5265c6c7f58476277cfe1
衣沾不足惜 <javascript.h@qq.com>
2021-12-17 18:49:10 +0800

    fix: 新建指令多出冒号
    Signed-off-by: 衣沾不足惜 <javascript.h@qq.com>

--- js/tool/featKeyWord.js
+++ js/tool/featKeyWord.js
@@ -4,9 +4,9 @@ const reSearchRepo = /^(Repo|Project|仓库):/i;
 const reSearchMember = /^(Member|成员|用户):/i;
 const reSearchCode = /^(Code|代码):/i;
 const reSearchDoc = /^(doc|文档|知识库):/i;
-const reNewIssue = /^(New Issue|新建任务):/i;
-const reNewPR = /^(New PR|新建评审):/i;
-const reNewRepo = /^(New Project|New Repo|新建仓库):/i;
+const reNewIssue = /^(New Issue|新建任务)/i;
+const reNewPR = /^(New PR|新建评审)/i;
+const reNewRepo = /^(New Project|New Repo|新建仓库)/i;
 
 export {
   reSearchIssue,
