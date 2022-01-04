598a102a0ab1d3360da9c34bcc6e931d319c43bd
衣沾不足惜 <javascript.h@qq.com>
2022-01-04 21:09:49 +0800

    perf: 取消未使用的权限

--- js/background.js
+++ js/background.js
@@ -17,6 +17,7 @@
 //     });
 //   }
 // });
+// webNavigation 权限已取消
 // chrome.webNavigation.onBeforeNavigate.addListener(function (tab) {
 //   console.log(tab);
 //   var tabUrl = tab.url;
@@ -27,14 +28,14 @@
 //   }
 // });
 
-import "./bg-omnibox.js";
-import { getStorage } from "./utils/storage";
-import { markNotice, getNotices } from "./tool/api";
+import './bg-omnibox.js';
+import { getStorage } from './utils/storage';
+import { markNotice, getNotices } from './tool/api';
 const sendNotification = function ({ message, url, updated_at, messageId }, btnText) {
   const opt = {
-    type: "basic",
-    iconUrl: "../img/favicon.ico",
-    title: "Gitee 通知",
+    type: 'basic',
+    iconUrl: '../img/favicon.ico',
+    title: 'Gitee 通知',
     message: message,
     priority: 2, // 优先级，从 -2 到 2，-2 优先级最低，2 最高，默认为零。
     eventTime: new Date(updated_at).getTime(),
@@ -42,9 +43,9 @@ const sendNotification = function ({ message, url, updated_at, messageId }, btnT
     silent: true, // true则不发出声音
     buttons: [
       {
-        title: btnText || "查看详情",
-      },
-    ],
+        title: btnText || '查看详情'
+      }
+    ]
   };
   chrome.notifications.create(JSON.stringify({ updated_at, url, messageId }), opt, e => {
     console.log(e);
@@ -52,7 +53,7 @@ const sendNotification = function ({ message, url, updated_at, messageId }, btnT
 };
 chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
   let oInfo = JSON.parse(notificationId);
-  let url = oInfo.url || "/";
+  let url = oInfo.url || '/';
   let messageId = oInfo.messageId;
   chrome.tabs.create({ url: `https://gitee.com${url}`, active: true }, tab => {});
   messageId && markNotice(messageId);
@@ -71,10 +72,10 @@ const launcher = result => {
 let noticesTimer = null;
 const INTERVAL = 40000;
 const getAllNotices = function () {
-  Promise.all([getNotices("referer"), getNotices("messages"), getNotices("infos")])
+  Promise.all([getNotices('referer'), getNotices('messages'), getNotices('infos')])
     .then(values => {
       if (values[0].status >= 400) {
-        sendNotification({ message: values[0].message, url: "/login" }, "去登录");
+        sendNotification({ message: values[0].message, url: '/login' }, '去登录');
         // 暂时不取消轮询. todo: 如果取消轮询 需要判断用户已登录 ,再通知开启轮询
         // clearInterval(noticesTimer);
       } else {
@@ -93,7 +94,7 @@ const startLoop = () => {
   noticesTimer = setInterval(() => {
     if (hour >= 9 && hour < 19) {
       getAllNotices();
-      console.count("累计运行次数");
+      console.count('累计运行次数');
     } else {
       clearInterval(noticesTimer);
     }
@@ -103,9 +104,9 @@ startLoop();
 
 // 支持下载单个文件
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type === "download") {
+  if (request.type === 'download') {
     chrome.downloads.download({ url: request.value }, e => {
-      console.log("下载信息: ", e);
+      console.log('下载信息: ', e);
     });
     return;
   }
@@ -113,17 +114,17 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 // onUpdated在页面初始化时不会执行(刷新页面), url改变才执行
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (!changeInfo.url || changeInfo.url.indexOf("#") > -1) return;
+  if (!changeInfo.url || changeInfo.url.indexOf('#') > -1) return;
   if (changeInfo.url) {
     chrome.tabs.sendMessage(tabId, {
-      type: "url-change",
+      type: 'url-change'
     });
   }
 });
 
 // 首次安装
 chrome.runtime.onInstalled.addListener(async () => {
-  let storageEntInfo = await getStorage("select-enterprises");
+  let storageEntInfo = await getStorage('select-enterprises');
   console.log(storageEntInfo);
-  !storageEntInfo && chrome.tabs.create({ url: "html/options.html", active: false }, tab => {});
+  !storageEntInfo && chrome.tabs.create({ url: 'html/options.html', active: false }, tab => {});
 });
